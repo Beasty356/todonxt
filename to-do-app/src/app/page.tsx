764d15de98a67045af646f5720ee52e49b50ac49
Beasty356 <zobairf03@gmail.com>
2023-08-29 11:16:16 +0600

    Final prototype of todo list

--- to-do-app/src/app/page.tsx
+++ to-do-app/src/app/page.tsx
@@ -1,15 +1,34 @@
+import { TodoItem } from "@/components/TodoItem";
+import { prisma } from "@/db";
 import Link from "next/link";
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo(id:string, complete:boolean) {
+  "use server"
+
+ await prisma.todo.update({ where:{id},data:{complete}})
+}
+
+export default async function Home() {
+  const todos = await getTodos()
+  // await prisma.todo.create({data: { title: "test",complete: false}})
   return <>
     <header className="flex justify-between 
     items-center mb-4">
       <h1 className="text-5xl">Todos</h1>
-      <Link  href="\new">NEW</Link>
+      <Link
+        className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+         hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        href="\new">NEW</Link>
     </header>
 
-    <ul>
-
+    <ul className="pl-4">
+      {todos.map(todo => (
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+      ))}
     </ul>
   </>
 }
